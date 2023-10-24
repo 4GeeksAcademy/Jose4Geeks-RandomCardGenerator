aba7e0282b139d50b756085e81db5eb64a0db114
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2023-10-24 01:05:13 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,49 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let suitsArray = ["♦", "♥", "♠", "♣"];
+let faceArray = [
+  "K",
+  "Q",
+  "J",
+  "10",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "A"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let topSuitElement = document.querySelector(".topsuit");
+  let faceElement = document.querySelector(".number");
+  let bottomSuitElement = document.querySelector(".bottomsuit");
+
+  const createRandomCard = () => {
+    let randomFaceNumberIndex = Math.floor(Math.random() * faceArray.length);
+    let randomSuitIndex = Math.floor(Math.random() * suitsArray.length);
+
+    topSuitElement.innerHTML = suitsArray[randomSuitIndex];
+    faceElement.innerHTML = faceArray[randomFaceNumberIndex];
+    bottomSuitElement.innerHTML = suitsArray[randomSuitIndex];
+
+    let randomSuit = suitsArray[randomSuitIndex];
+    if (randomSuit === "♥" || randomSuit === "♦") {
+      document.querySelectorAll(".suitColor").forEach(suitElement => {
+        suitElement.style.color = "red";
+      });
+    } else {
+      document.querySelectorAll(".suitColor").forEach(suitElement => {
+        suitElement.style.color = "black";
+      });
+    }
+  };
+  createRandomCard();
+  document.getElementById("GenerateCard").addEventListener("click", function() {
+    createRandomCard();
+  });
 };
